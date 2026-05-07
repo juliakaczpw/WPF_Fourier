488c3ab94ec88af85d6f703a584504f3e638c201
Julia Kaczyńska <juliakaczynska.pw@gmail.com>
2026-05-08 00:26:49 +0200

    basics

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Fourier
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fourier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void NewMenuItem(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenMenuItem(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveMenuItem(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
