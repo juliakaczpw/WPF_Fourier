1c674f8939fef11af149fc6cc1913067486fe434
Julia Kaczyńska <juliakaczynska.pw@gmail.com>
2026-05-08 10:21:01 +0200

    canvas statyczne kolka

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.Text;
 using System.Windows;
@@ -16,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
+
 namespace Fourier
 {
     /// <summary>
@@ -29,9 +29,20 @@ namespace Fourier
         private Stopwatch stopwatch;
         private const double TotalSeconds = 10.0;
 
-        private Bitmap drawingBitmap;
+        
         private const int CanvasWidth = 900;
         private const int CanvasHeight = 600;
+
+        //listy kontrolek na canvas zeby moc aktualizowac a kazdym ticku
+        private List<Ellipse> circleShapes = new List<Ellipse>();
+        private List<Line> radiusLines = new List<Line>();
+        private Ellipse penDot;
+        private Polyline penTrail;
+
+        
+        // flagi opcji
+        private bool drawCircles = true;
+        private bool drawLines = true;
 
         private bool circleVisible = false;
 
@@ -59,10 +70,101 @@ namespace Fourier
 
             DataContext = this;
 
-            //inicjalizujemy bitmapy
-            drawingBitmap = new Bitmap(CanvasWidth, CanvasHeight);
-            ClearBitmap();
-            UpdateImage();
+            
+        }
+
+        private void BuildShapes()
+        {
+            //czyscimy
+            PlotterCanvas.Children.Clear();
+            circleShapes.Clear();
+            radiusLines.Clear();
+
+            //okregi pomocznicze
+            foreach (var c in Circles)
+            {
+                var ellipse = new Ellipse
+                {
+                    Width = c.Radius * 2,
+                    Height = c.Radius * 2,
+                    Stroke = Brushes.Gray,
+                    StrokeThickness = 1
+                };
+                circleShapes.Add(ellipse);
+                if (drawCircles)
+                    PlotterCanvas.Children.Add(ellipse);
+            }
+
+            //linie promienie
+            foreach (var c in Circles)
+            {
+                var line = new Line
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 1
+                };
+                radiusLines.Add(line);
+                if (drawLines)
+                    PlotterCanvas.Children.Add(line);
+            }
+
+            //kreska lamana niebieska
+            penTrail = new Polyline
+            {
+                Stroke = Brushes.Blue,
+                StrokeThickness = 2
+            };
+            PlotterCanvas.Children.Add(penTrail);
+
+            //pendot czerwony
+            penDot = new Ellipse
+            {
+                Width = 6,
+                Height = 6,
+                Fill = Brushes.Red
+            };
+            PlotterCanvas.Children.Add(penDot);
+        }
+
+        private void UpdatePositions(double t)
+        {
+            if (Circles.Count == 0) return;
+
+            //srodek canvasu - first okrag
+            Point prev = new Point(PlotterCanvas.ActualWidth / 2, PlotterCanvas.ActualHeight / 2);
+
+            for (int i = 0; i < Circles.Count; i++)
+            {
+                var c = Circles[i];
+
+                //okrag-pomocniczny,, srodek = prev, lewy gorny rog = prev-r
+                Canvas.SetLeft(circleShapes[i], prev.X - c.Radius);
+                Canvas.SetTop(circleShapes[i], prev.Y - c.Radius);
+                circleShapes[i].Width = c.Radius * 2;
+                circleShapes[i].Height = c.Radius * 2;
+
+                //koniec promienia - theta
+                double theta = 2 * Math.PI * c.Frequency * (t / TotalSeconds);
+                Point next = new Point(
+                    prev.X + c.Radius * Math.Cos(theta),
+                    prev.Y + c.Radius * Math.Sin(theta)
+                );
+
+                //ustawiam linie promiec od prev do next
+                radiusLines[i].X1 = prev.X;
+                radiusLines[i].Y1 = prev.Y;
+                radiusLines[i].X2 = next.X;
+                radiusLines[i].Y2 = next.Y;
+
+                prev = next;  //next okrag ma srodek tu
+            }
+
+            //pen na koncu ostatniego promienia
+            Canvas.SetLeft(penDot, prev.X - penDot.Width / 2);
+            Canvas.SetTop(penDot, prev.Y - penDot.Height / 2);
+
+            //aktualna pozycja piora do sladu
+            penTrail.Points.Add(prev);
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -79,9 +181,7 @@ namespace Fourier
                 //rysujemy kolo o promieniu pierwszego el z kolekcjio
                 if (Circles.Count > 0)
                 {
-                    ClearBitmap();
-                    DrawCircle(Circles[0].Radius);
-                    UpdateImage();
+                    
                     circleVisible = true;
                 }
                 return;
@@ -90,46 +190,8 @@ namespace Fourier
             MainProgressBar.Value = percent;
         }
 
-        private void ClearBitmap()
-        {
-            using (Graphics g = Graphics.FromImage(drawingBitmap))
-            {
-                g.Clear(System.Drawing.Color.White);
-            }
-        }
+       
 
-        private void DrawCircle(double radius)
-        {
-            using (Graphics g = Graphics.FromImage(drawingBitmap))
-            {
-                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-
-                float r = (float)radius;
-                float cx = CanvasWidth / 2f;
-                float cy = CanvasHeight / 2f;
-
-                //drawellipse rysuje elipse wpisana w prostokat, wiec daje lewy-gorny rog i jego width/height
-                g.DrawEllipse(System.Drawing.Pens.Black, cx - r, cy - r, 2 * r, 2 * r);
-            }
-        }
-
-        private void UpdateImage()
-        {
-            using (MemoryStream ms = new MemoryStream())
-            {
-                drawingBitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
-                ms.Position = 0;
-
-                BitmapImage bmp = new BitmapImage();
-                bmp.BeginInit();
-                bmp.CacheOption = BitmapCacheOption.OnLoad;
-                bmp.StreamSource = ms;
-                bmp.EndInit();
-                bmp.Freeze();
-
-                PlotterImage.Source = bmp;
-            }
-        }
 
         private void Circle_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
@@ -140,9 +202,7 @@ namespace Fourier
             //i tylko jak zmieni sie pierwszy el kolekcji cicrcles[0]
             if (sender == Circles[0] && e.PropertyName == nameof(Circle.Radius))
             {
-                ClearBitmap();
-                DrawCircle(Circles[0].Radius);
-                UpdateImage();
+                
             }
         }
 
@@ -169,6 +229,12 @@ namespace Fourier
 
         private void StartButtonItem(object sender, RoutedEventArgs e)
         {
+            if (stopwatch.ElapsedMilliseconds == 0)
+            {
+                //start od nowa
+                BuildShapes();
+                UpdatePositions(0); //position poczatkowy
+            }
             timer.Start();
             stopwatch.Start();
 
@@ -184,12 +250,15 @@ namespace Fourier
         private void ResetButtonItem(object sender, RoutedEventArgs e)
         {
             timer.Stop();
-            stopwatch.Reset(); //zeruje nie wznawia
-            MainProgressBar.Value = 0; //pasek na zero
+            stopwatch.Reset();
+            MainProgressBar.Value = 0;
 
-            ClearBitmap();
-            UpdateImage();
-            circleVisible = false;
+            //czyscimy
+            PlotterCanvas.Children.Clear();
+            circleShapes.Clear();
+            radiusLines.Clear();
+            penTrail = null;
+            penDot = null;
         }
     }
 }
