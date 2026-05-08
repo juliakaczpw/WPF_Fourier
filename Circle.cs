040f17e76717a8b33ccbe9969b96db40b47c64eb
Julia Kaczyńska <juliakaczynska.pw@gmail.com>
2026-05-08 08:43:30 +0200

    data binding

---
+++ Circle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fourier
+{
+    public class Circle
+    {
+        public double Radius { get; set; }
+        public double Time { get; set; }
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace Fourier
 {
@@ -16,14 +17,27 @@ namespace Fourier
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Circle> Circles { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Circles = new ObservableCollection<Circle>
+            {
+                new Circle { Radius = 100, Time = 1 },
+                new Circle { Radius = 5, Time = 1.54 },
+                new Circle { Radius = 100, Time = 0.1 },
+                new Circle { Radius = 10, Time = 1 }
+            };
+
+            DataContext = this;
         }
 
         private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+
         }
 
         private void NewMenuItem(object sender, RoutedEventArgs e)
